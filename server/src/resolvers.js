670ca2b222973a3da5455e717d662923ab941877
Miguel Lorenzi <miguelml159357@gmail.com>
2023-04-26 11:18:38 -0300

    Alteração modules

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -15,7 +15,6 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-
     modules: ({id}, _, {dataSources}) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
